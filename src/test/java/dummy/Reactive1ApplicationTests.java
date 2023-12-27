53c625648e2e7fc7c3249267bd3743cd5f107b1f
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-12-27 15:41:22 +0530

    Create Reactive1ApplicationTests.java

---
+++ src/test/java/dummy/Reactive1ApplicationTests.java
@@ -0,0 +1,13 @@
+package dummy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reactive1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
